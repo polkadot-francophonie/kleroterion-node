0ed71f60b88005697b27a7f2938e8556f21f8da3
claudiociardelli <claudio.ciardelli@gmail.com>
2021-09-24 17:08:19 +0000

    Benchmarking corrections

--- pallets/kleroterion/src/benchmarking.rs
+++ pallets/kleroterion/src/benchmarking.rs
@@ -1,17 +1,20 @@
 //! Benchmarking setup for pallet-kleroterion
+#[cfg(feature = "runtime-benchmarks")]
 
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as Kleroterion;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	fn bench_open_jury_call_100(b: &mut Bencher) {
+	// Trigger 100 Open Jury Calls
+	bench_open_jury_call_100  {
+		let s in 0 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
 		let tribes = vec![b"Tribe1".to_vec(), b"Tribe2".to_vec()];
-		Kleroterion::open_jury_call(Origin::signed(1), tribes.clone(), 1, UX_TS_20300101)
-    });
+	}: 	Kleroterion::open_jury_call(RawOrigin::Signed(caller), tribes.clone(), 1, UX_TS_20300101);
 }
 
-impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+impl_benchmark_test_suite!(Kleroterion, crate::mock::new_test_ext(), crate::mock::Test);
